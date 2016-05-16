b6c5deca4c328392b59dde7f76150035b85d90a1
杉山卓彌 <dioxiy@yahoo.co.jp>
2016-05-16 17:31:06 +0900

    google_map_fresckckckck

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,25 +1,102 @@
 window.onload = function(){
-// キャンパスの要素を取得する
-var canvas = document.getElementById( 'map-canvas' ) ;
 
-// 中心の位置座標を指定する
-var latlng = new google.maps.LatLng(35.8488658 , 139.603112);
+if(document.URL.match("/schedules/1")) {
+    var canvas = document.getElementById( 'map-canvas' ) ;
 
-// 地図のオプションを設定する
-var mapOptions = {
-  zoom: 15 ,        // ズーム値
-  center: latlng ,    // 中心座標 [latlng]
-};
+    // 中心の位置座標を指定する
+    var latlng = new google.maps.LatLng(35.8488658 , 139.603112);
 
-// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-var map = new google.maps.Map( canvas , mapOptions ) ;
+    // 地図のオプションを設定する
+    var mapOptions = {
+      zoom: 15 ,        // ズーム値
+      center: latlng ,    // 中心座標 [latlng]
+    };
 
-// マーカーのインスタンスは配列で管理しよう
-var markers = [] ;
+    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+    var map = new google.maps.Map( canvas , mapOptions ) ;
 
-// マーカーのインスタンスを作成する
-markers[0] = new google.maps.Marker({
-  map: map ,
-  position: new google.maps.LatLng(35.8488658 , 139.603112) ,
-}) ;
+    // マーカーのインスタンスは配列で管理しよう
+    var markers = [] ;
+
+    // マーカーのインスタンスを作成する
+    markers[0] = new google.maps.Marker({
+      map: map ,
+      position: new google.maps.LatLng(35.8488658 , 139.603112) ,
+    }) ;
+  };
+
+if(document.URL.match("/schedules/2")) {
+    var canvas = document.getElementById( 'map-canvas' ) ;
+
+    // 中心の位置座標を指定する
+    var latlng = new google.maps.LatLng(35.8488658 , 139.603112);
+
+    // 地図のオプションを設定する
+    var mapOptions = {
+      zoom: 15 ,        // ズーム値
+      center: latlng ,    // 中心座標 [latlng]
+    };
+
+    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+    var map = new google.maps.Map( canvas , mapOptions ) ;
+
+    // マーカーのインスタンスは配列で管理しよう
+    var markers = [] ;
+
+    // マーカーのインスタンスを作成する
+    markers[0] = new google.maps.Marker({
+      map: map ,
+      position: new google.maps.LatLng(35.8488658 , 139.603112) ,
+    }) ;
+  };
+
+  if(document.URL.match("/schedules/12")) {
+    var canvas = document.getElementById( 'map-canvas' ) ;
+
+    // 中心の位置座標を指定する
+    var latlng = new google.maps.LatLng(36.0574854 , 139.4696577);
+
+    // 地図のオプションを設定する
+    var mapOptions = {
+      zoom: 15 ,        // ズーム値
+      center: latlng ,    // 中心座標 [latlng]
+    };
+
+    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+    var map = new google.maps.Map( canvas , mapOptions ) ;
+
+    // マーカーのインスタンスは配列で管理しよう
+    var markers = [] ;
+
+    // マーカーのインスタンスを作成する
+    markers[0] = new google.maps.Marker({
+      map: map ,
+      position: new google.maps.LatLng(36.0574854 , 139.4696577) ,
+    }) ;
+  };
+
+  if(document.URL.match("/schedules/22")) {
+    var canvas = document.getElementById( 'map-canvas' ) ;
+
+    // 中心の位置座標を指定する
+    var latlng = new google.maps.LatLng(36.0574854 , 139.4696577);
+
+    // 地図のオプションを設定する
+    var mapOptions = {
+      zoom: 15 ,        // ズーム値
+      center: latlng ,    // 中心座標 [latlng]
+    };
+
+    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+    var map = new google.maps.Map( canvas , mapOptions ) ;
+
+    // マーカーのインスタンスは配列で管理しよう
+    var markers = [] ;
+
+    // マーカーのインスタンスを作成する
+    markers[0] = new google.maps.Marker({
+      map: map ,
+      position: new google.maps.LatLng(36.0574854 , 139.4696577) ,
+    }) ;
+  };
 }
