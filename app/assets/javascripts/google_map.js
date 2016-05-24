e19f0144784ba00d9dd4e13904be85136027fe56
杉山卓彌 <dioxiy@yahoo.co.jp>
2016-05-24 15:07:08 +0900

    google_map_new_002

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -75,57 +75,7 @@ if(document.URL.match("/schedules/2")) {
     }) ;
   };
 
-  if(document.URL.match("/schedules/22")) {
-    var canvas = document.getElementById( 'map-canvas' ) ;
-
-    // 中心の位置座標を指定する
-    var latlng = new google.maps.LatLng(36.0574854 , 139.4696577);
-
-    // 地図のオプションを設定する
-    var mapOptions = {
-      zoom: 15 ,        // ズーム値
-      center: latlng ,    // 中心座標 [latlng]
-    };
-
-    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-    var map = new google.maps.Map( canvas , mapOptions ) ;
-
-    // マーカーのインスタンスは配列で管理しよう
-    var markers = [] ;
-
-    // マーカーのインスタンスを作成する
-    markers[0] = new google.maps.Marker({
-      map: map ,
-      position: new google.maps.LatLng(36.0574854 , 139.4696577) ,
-    }) ;
-  };
-
-  if(document.URL.match("/schedules/32")) {
-    var canvas = document.getElementById( 'map-canvas' ) ;
-
-    // 中心の位置座標を指定する
-    var latlng = new google.maps.LatLng(36.0574854 , 139.4696577);
-
-    // 地図のオプションを設定する
-    var mapOptions = {
-      zoom: 15 ,        // ズーム値
-      center: latlng ,    // 中心座標 [latlng]
-    };
-
-    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-    var map = new google.maps.Map( canvas , mapOptions ) ;
-
-    // マーカーのインスタンスは配列で管理しよう
-    var markers = [] ;
-
-    // マーカーのインスタンスを作成する
-    markers[0] = new google.maps.Marker({
-      map: map ,
-      position: new google.maps.LatLng(36.0574854 , 139.4696577) ,
-    }) ;
-  };
-
-  if(document.URL.match("/schedules/42")) {
+  if(document.URL.match("/schedules/22") && document.URL.match("/schedules/32") && document.URL.match("/schedules/42")) {
     var canvas = document.getElementById( 'map-canvas' ) ;
 
     // 中心の位置座標を指定する
